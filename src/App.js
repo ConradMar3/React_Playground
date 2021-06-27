8c0b9217726db1fb82932e520554cdf74e7264bb
ConradMare890317 <Conradmare@gmail.com>
2021-06-27 15:19:50 +0200

    2021/06/27 (pm) 3-axis  * 5.
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo1" alt="logo" />
+        <img src={logo} className="App-logo2" alt="logo" />
+        <img src={logo} className="App-logo3" alt="logo" />
+        <p>
+          Code_playground/sandbox.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React.Js/Three-js/React-three-fibre.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
