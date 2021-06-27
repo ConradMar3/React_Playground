bed075eea399e0a118e1f0e8538fc7b2aea51e43
ConradMare890317 <Conradmare@gmail.com>
2021-06-27 19:36:57 +0200

    2021/06/27 (pm) react-three-fibre'-ish'.
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import logo192 from './image/logo192.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo1" alt="logo" />
         <img src={logo} className="App-logo2" alt="logo" />
         <img src={logo} className="App-logo3" alt="logo" />
-        <p>
-          Code_playground/sandbox.
-        </p>
+        <img src={logo} className="App-logo4" alt="logo" />
         <a
           className="App-link"
           href="https://github.com/ConradMare890317"
           target="_blank"
           rel="noopener noreferrer"
         >
-          React.Js/Three-js/React-three-fibre.
+          React.Js-Animate.Keyframes
         </a>
       </header>
     </div>
