9f4f2d9a0b12a8d61f2dccdb7d4c72e0fda7a384
ConradMare890317 <Conradmare@gmail.com>
2021-07-06 20:31:03 +0200

    2021/07/06 (pm)
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,26 +6,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo2} className="App-logo12" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo1" alt="logo" />
         <img src={logo} className="App-logo2" alt="logo" />
-        <img src={logo2} className="App-logo3" alt="logo" />
-        <img src={logo2} className="App-logo3" alt="logo" />
-        <img src={logo2} className="App-logo3" alt="logo" />
-        <img src={logo2} className="App-logo4" alt="logo" />
-        <img src={logo2} className="App-logo5" alt="logo" />
+        <img src={logo} className="App-logo3" alt="logo" />
+        <img src={logo} className="App-logo4" alt="logo" />
+        <img src={logo} className="App-logo5" alt="logo" />
         <img src={logo2} className="App-logo6" alt="logo" />
         <img src={logo2} className="App-logo7" alt="logo" />
         <img src={logo2} className="App-logo8" alt="logo" />
-        <img src={logo2} className="App-logo7" alt="logo" />
-        <img src={logo2} className="App-logo8" alt="logo" />
-        <img src={logo} className="App-logo9" alt="logo" />
-        <img src={logo} className="App-logo10" alt="logo" />
-        <img src={logo} className="App-logo11" alt="logo" />
-        <img src={logo} className="App-logo9" alt="logo" />
-        <img src={logo} className="App-logo10" alt="logo" />
+        <img src={logo2} className="App-logo9" alt="logo" />
+        <img src={logo2} className="App-logo10" alt="logo" />
+        <img src={logo2} className="App-logo11" alt="logo" />
+        
         <a
           className="App-link"
           href="https://github.com/ConradMare890317"
