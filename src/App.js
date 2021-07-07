704b04c61491e2992d8c5321b305ec0af394f5f1
ConradMare890317 <Conradmare@gmail.com>
2021-07-07 17:21:00 +0200

    2021/07/07 (pm)
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,27 +6,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src={logo2} className="App-logo12" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo1" alt="logo" />
         <img src={logo} className="App-logo2" alt="logo" />
-        <img src={logo} className="App-logo3" alt="logo" />
-        <img src={logo} className="App-logo4" alt="logo" />
-        <img src={logo} className="App-logo5" alt="logo" />
-        <img src={logo2} className="App-logo6" alt="logo" />
-        <img src={logo2} className="App-logo7" alt="logo" />
-        <img src={logo2} className="App-logo8" alt="logo" />
+        <img src={logo2} className="App-logo3" alt="logo" />
+        <img src={logo2} className="App-logo4" alt="logo" />
+        <img src={logo2} className="App-logo5" alt="logo" />
+        <img src={logo} className="App-logo6" alt="logo" />
+        <img src={logo} className="App-logo7" alt="logo" />
+        <img src={logo} className="App-logo8" alt="logo" />
         <img src={logo2} className="App-logo9" alt="logo" />
         <img src={logo2} className="App-logo10" alt="logo" />
         <img src={logo2} className="App-logo11" alt="logo" />
-        <p>Animate Keyframes</p>
+        <p>Animated @keyframes</p>
         <a
           className="App-link"
           href="https://github.com/ConradMare890317"
           target="_blank"
           rel="noopener noreferrer"
         >
-          React.Js-Animate.Keyframes
+          React - Playground
         </a>
       </header>
     </div>
